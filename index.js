b02286ad9d4f8bf9451052dd989f83906090860a
lucky <takuto.yoshikai@gmail.com>
2021-12-27 12:44:13 +0900

    backup

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const { encrypt, decrypt } = require("aes256cbc");
 
-const password = "";
+const contractAddress = "0x799358B503325af18FCBF9DE723393Ce76B1bfAF";
+const baseUrl = "http://localhost:3000";
+const rpcHost = "https://matic-mumbai.chainstacklabs.com";
+
+const crypto = require("crypto");
+const Accounts = require("web3-eth-accounts");
+const accounts = new Accounts(rpcHost);
+
+const IpfsHttpClient = require("ipfs-http-client");
+const ipfsClient = IpfsHttpClient({
+  host: "ipfs.infura.io",
+  port: 5001,
+  protocol: "https",
+});
 
 async function fetchFile(ipfsHash) {
   const response = await ipfsClient.get(ipfsHash);
@@ -11,6 +24,14 @@ async function fetchFile(ipfsHash) {
     }
     return content;
   }
+}
+
+async function upload(data) {
+  const response = await ipfsClient.add(data);
+  if (response) {
+    return response.path;
+  }
+  throw new Error("Failed to upload the file.");
 }
 
 
@@ -43,4 +64,70 @@ async function fetchBackup(backupList, password) {
     });
   }
   return files;
+}
+
+function getPolygonPrivateKey(silverKey) {
+  const hashHex = crypto.createHash("sha256").update(silverKey, "utf8").digest("hex");
+  return hashHex;
+}
+
+function privateKeyToAddress(privateKey) {
+  return accounts.privateKeyToAccount(privateKey).address;
+}
+
+async function polygonGet(privateKey) {
+  const address = privateKeyToAddress(privateKey);
+  const baseUrl = "http://localhost:3000";
+  const client = new Client(rpcHost, accounts, contractAddress, baseUrl);
+  await client.init();
+
+  const ipfsHashes = await client.getIpfsHashes(address);
+  return ipfsHashes[ipfsHashes.length - 1].ipfsHash;
+}
+
+async function polygonSave(ipfsHash, privateKey) {
+  const client = new Client(rpcHost, accounts, contractAddress, baseUrl);
+  await client.init();
+  const signed = await client.signIpfsHash(ipfsHash, {
+    privateKey,
+  });
+  await client.createBackup(signed);
+}
+
+function createBackupList(encryptedFiles) {
+  let backupList = "";
+  for (const encryptedFile of encryptedFiles) {
+    backupList += encryptedFile.ipfsHash + " " + name + "\n";
+  }
+  if (backupList.endsWith("\n")) {
+    backupList = backupList.slice(0, -1);
+  }
+  return backupList;
+}
+
+async function backup(files, password) {
+  const encryptedFiles = [];
+  for (const file of files) {
+    const encryptedData = encrypt(file.content, password);
+    const ipfsHash = await upload(encryptedData);
+    encryptedFiles.push({
+      name: file.name,
+      ipfsHash,
+    });
+  }
+  const backupList = createBackupList(encryptedFiles);
+  const encryptedBackupList = encrypt(Buffer.from(backupList), password);
+  const backupIpfsHash = await upload(encryptedBackupList);
+  const privateKey = getPolygonPrivateKey(password);
+  await polygonSave(backupIpfsHash, privateKey);
+}
+
+module.exports = {
+  polygonGet,
+  polygonSave,
+  fetchFile,
+  fetchBackupList,
+  fetchBackup,
+  getPolygonPrivateKey,
+  backup,
 }
