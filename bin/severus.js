dd994f46eaa459fa0ef59683008124fcc89a2167
lucky <takuto.yoshikai@gmail.com>
2021-12-28 18:53:32 +0900

    severus command

---
+++ bin/severus.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+
+const { backup, restore } = require("../");
+
+const userHome = process.env[process.platform == "win32" ? "USERPROFILE" : "HOME"];
+const fs = require("fs");
+const path = require("path");
+const silverKeyPath = path.join(userHome, ".silver", "secret", "private-key.txt");
+
+const silverKey = fs.readFileSync(silverKeyPath, "utf8").trim();
+
+function readdirRecursively (dir, files = []) {
+  const paths = fs.readdirSync(dir);
+  const dirs = [];
+  for (const _path of paths) {
+    const stats = fs.statSync(path.join(dir, _path));
+    if (stats.isDirectory()) {
+      dirs.push(path.join(dir, _path));
+    } else {
+      files.push(path.join(dir, _path));
+    }
+  }
+  for (const d of dirs) {
+    files = readdirRecursively(d, files);
+  }
+  const resolvedDir = path.resolve(dir);
+  return files;
+};
+
+function allFiles(dirPath) {
+  if (dirPath.endsWith("/")) {
+    dirPath = dirPath.slice(0, -1);
+  }
+  const resolvedDirPath = path.resolve(dirPath);
+  return readdirRecursively(dirPath).map(file => {
+    if (file.startsWith(resolvedDirPath)) {
+      return "." + file.slice(resolvedDirPath.length);
+    }
+    return file;
+  });
+}
+
+(async () => {
+  if (process.argv[2] === "restore") {
+    const files = await restore(silverKey);
+    for (const file of files) {
+      const p = path.resolve(path.join(process.cwd(), file.name));
+      if (!p.startsWith(process.cwd())) {
+        continue;
+      }
+      fs.writeFileSync(p, file.content);
+    }
+  } else if (process.argv[2] === "save") {
+    if (process.argv.length < 4) {
+      return;
+    }
+    const dirPath = process.argv[3];
+    const filePaths = allFiles(dirPath);
+    const files = filePaths.map(filePath => {
+      const content = fs.readFileSync(filePath);
+      return {
+        name: filePath,
+        content,
+      }
+    });
+    await backup(files, silverKey);
+  }
+})();
